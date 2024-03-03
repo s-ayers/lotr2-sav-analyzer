e2cbbf3fc3e3964dba18fe0c63c726477746888f
bluegeek9 <5614-bluegeek9@users.noreply.drupalcode.org>
2024-03-02 19:28:01 -0600

    Orthogonal images export

--- pl8.js
+++ pl8.js
@@ -1,63 +1,111 @@
 import YAML from 'yaml';
 import fs from 'fs';
-import { Parser} from 'binary-parser';
+import {Parser} from 'binary-parser';
+import {PNG} from "pngjs";
+import { parse } from 'path';
 
-const file_def = fs.readFileSync('./pl8.yml', 'utf8');
-const def = YAML.parse(file_def);
+const ORTHOGONAL = 0;
+const RUN_LENGTH_ENCODED = 1;
 
-console.log(def.file);
-const tile = new Parser()
-  .endianess("little")
-  .uint16("width")
-  .uint16("height")
-  .uint32("offset")
-  .uint16("x")
-  .uint16("y")
-  .uint8("extraType")
-  .uint8("extraRows")
-  .uint16("unknown_000");
 
-const yml = new Parser().endianess("little");
-for (const key in def.file.mapping) {
-  const item = def.file.mapping[key];
-  const type = item.type;
+function write_image(parsed) {
+  const options = { width: 640, height: 480 };
+  const image = new PNG(options);
 
-  switch (type) {
-    case 'array':
-      console.log('yes');
-      if (item.array === 'tile') {
-        yml.array(key, {
-          type: tile,
-          length: item.length
-        });
+  for (const tile of parsed.sprites) {
 
+  }
+
+}
+
+/**
+ * Convert an orthogonal sprite to an image buffer.
+ */
+function orthogonal(sprite, pl8_buffer, palette_buffer) {
+  const offset = sprite.offset;
+  const length = sprite.width * sprite.height;
+
+  const buffer = pl8_buffer.subarray(offset, offset + length);
+
+  const options = { width: sprite.width, height: sprite.height };
+  const image = new PNG(options);
+  for (let h = 0; h < sprite.height; h+=1) {
+    for (let w = 0; w < sprite.width; w+=1) {
+      const buffer_index = (h * sprite.width + w);
+      const color = buffer.readUInt8(buffer_index);
+      const image_index = buffer_index * 4;
+
+      if (color === 0) {
+        image.data[image_index] = 0x00;
+        image.data[image_index + 1] = 0x00;
+        image.data[image_index + 2] = 0x00;
+        image.data[image_index + 3] = 0x00;
+        continue;
       }
+
+      image.data[image_index] = palette_buffer.readUInt8(color * 3) * 4;
+      image.data[image_index + 1] = palette_buffer.readUInt8(color * 3 + 1) * 4;
+      image.data[image_index + 2] = palette_buffer.readUInt8(color * 3 + 2) * 4;
+      image.data[image_index + 3] = 0xff;
+    }
+  }
+
+
+  return image;
+}
+
+function run_length_encoded() {
+
+}
+
+const sprite = new Parser().endianess('little')
+  .uint16('width')
+  .uint16('height')
+  .uint32('offset')
+  .uint16('x')
+  .uint16('y')
+  .uint8('extra_type')
+  .uint8('extra_rows')
+  .uint16('unknown');
+
+const pl8 = new Parser().endianess('little')
+  .uint16('type')
+  .uint16('count')
+  .int8('unknown_000')
+  .int8('unknown_001')
+  .int8('unknown_002')
+  .int8('unknown_003')
+  .array('sprites', {
+    type: sprite,
+    length: 'count'
+  });
+
+
+const pl8_buffer = fs.readFileSync('./images/Arm_swor.pl8');
+const palette_buffer = fs.readFileSync('./images/Armoury.256');
+
+const parsed = pl8.parse(pl8_buffer);
+
+const pad = (parsed.count + '').length;
+const type = parsed.type;
+
+let i = 0;
+for (const tile of parsed.sprites) {
+  switch (type) {
+    case ORTHOGONAL:
+      tile.image = orthogonal(tile, pl8_buffer, palette_buffer);
+      const counter = i.toString().padStart(pad, '0');
+      tile.image.pack().pipe(fs.createWriteStream(`sprites/Arm_swor/Arm_swor_${counter}.png`));
+      break;
+
+    case RUN_LENGTH_ENCODED:
       break;
 
     default:
-      yml[item.type](key);
-      console.log(type);
-      break;
+      throw new Error(`Unknown type: ${type}`);
   }
-  // console.log(yml);
-  // console.log(key);
-  // console.log(def.pl8.mapping[key]);
+
+  i += 1;
 }
 
-
-const pl8 = new Parser()
-  .useContextVars()
-  .endianess("little")
-  .uint16("type")
-  .uint16("tile_count")
-  .uint32("unknown_000");
-
-const file_buffer = fs.readFileSync('./images/A2_horse.pl8');
-
-
-
-
-const parsed = pl8.parse(file_buffer);
-
-console.log(parsed);
-console.log(yml.parse(file_buffer));
+// write_image(parsed).pack().pipe(fs.createWriteStream(`sprites/Arm_swor.png`));
